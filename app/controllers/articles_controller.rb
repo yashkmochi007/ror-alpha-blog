01e312fb8f8ed178e90eabfebaa15f7edbe278c9
Yash Mochi <yashkmochi007@gmail.com>
2020-04-13 19:35:41 +0530

    Implemeneted view and create article functionality

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,22 @@
+class ArticlesController < ApplicationController
+
+  def show
+    @article = Article.find(params[:id])
+    # @article = { "title" => 'title', "description" => "sample desc"}
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
-
   get 'about', to: 'pages#about'
+  resources :articles, only: [:index, :show, :create, :new]
 end

e2ee8cf0cc76d1de198aabae6c3e2352bc3b59c3
Yash Mochi <yashkmochi007@gmail.com>
2020-04-27 20:50:46 +0530

    Added Users crud and associated articles with user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-  
+
   def show
   end
 
@@ -17,6 +17,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
@@ -39,7 +40,7 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
-  private 
+  private
 
   def set_article
     @article = Article.find(params[:id])
